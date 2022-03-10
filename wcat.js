5bd3a1547f658c2dd149edeae1e4b3fe452a9957
Kushal-sharma1 <sharma.k.kushal@gmail.com>
2022-03-10 23:58:27 +0530

    option 3

--- wcat.js
+++ wcat.js
@@ -1,79 +1,98 @@
-const fs  = require("fs");
+const fs = require("fs");
 //took input from terminal
 let input = process.argv.slice(2);
 // console.log(input);
 
-let filepath =[];
-let option=[];
+let filepath = [];
+let option = [];
 
-for(let i=0;i<input.length;i++){
-    if(input[i].charAt(0)=='-'){
-        option.push(input[i]);
-    }else{
-        filepath.push(input[i]);
-    }  
+for (let i = 0; i < input.length; i++) {
+  if (input[i].charAt(0) == "-") {
+    option.push(input[i]);
+  } else {
+    filepath.push(input[i]);
+  }
 }
 console.log(option);
 //checking file exist or not
 
-for(let i=0;i<filepath.length ;i++){
-    let exist =fs.existsSync(filepath[i]);
-    if(!exist){
-        console.log(filepath[i] +" File not exit");
-        return ;
-    }
+for (let i = 0; i < filepath.length; i++) {
+  let exist = fs.existsSync(filepath[i]);
+  if (!exist) {
+    console.log(filepath[i] + " File not exit");
+    process.exit();
+  }
 }
 
 //taking content from file
-let content="";
-for(let i=0;i<filepath.length;i++){
-    let temp = fs.readFileSync(filepath[i]);
-    content+=temp+"\r\n";
+let content = "";
+for (let i = 0; i < filepath.length; i++) {
+  let temp = fs.readFileSync(filepath[i]);
+  content += temp + "\r\n";
 }
 
-let contentarr= content.split("\r\n");
-console.table(contentarr);
-//now i am executing  option
-//option -s
-if(option.includes("-s")){
-     for(let i=1;i<contentarr.length;i++){
-         if(contentarr[i]=="" && (contentarr[i-1]== null || contentarr[i-1]=="")){
-             contentarr[i]=null;
-         }
-     }
-     
-     let temp=[];
-     for(let i=0;i<contentarr.length;i++){
-         if(contentarr[i]!=null){
-             temp.push(contentarr[i]);
-         }
-     }
+let contentarr = content.split("\r\n");// console.table(contentarr);
 
-     contentarr=temp;
-     console.table(contentarr);
+
+
+//checking an option
+//option -s
+if (option.includes("-s")) {
+  for (let i = 1; i < contentarr.length; i++) {
+    if (
+      contentarr[i] == "" &&
+      (contentarr[i - 1] == null || contentarr[i - 1] == "")
+    ) {
+      contentarr[i] = null;
+    }
+  }
+
+  let temp = [];
+  for (let i = 0; i < contentarr.length; i++) {
+    if (contentarr[i] != null) {
+      temp.push(contentarr[i]);
+    }
+  }
+
+  contentarr = temp;
+//   console.table(contentarr);
 }
 
 //option -n && option -b
-if(option.length >1 &&option[1]=="-n"){
-let num=1;
-for(let i=0;i<contentarr.length;i++){
-    contentarr[i]=num+" "+contentarr[i];
-    num++;
-}
-console.table(contentarr);
-}else if(option.lenght >1 && option[1]=="-b"){
-    let num=1;
-    for(let i=0;i<contentarr.length;i++){
-        if(contentarr[i]!=""){
-            contentarr[i]=num+" "+contentarr[i];
-            num++;
-        }
-    }
-    console.table(contentarr);  
-}
+// -n && -b not exist or only one exist or both exist
+let isn = option.includes("-n");
+let isb = option.includes("-b");
+if (isn == true && isb == false) {
+changeN();
+} else if (isn == false && isb == true) {
+changeB();
+} else if (isn == true && isb == true) {
+  if (option.indexOf("-n") < option.indexOf("-b")) {
+    changeN();
+  } else {
+    changeB();
+  }
 
-let finalcontent="";
-for(let i=0;i<contentarr.length;i++){
-    finalcontent+=(contentarr[i]+"\n");
+}
+function changeN(){
+    let num = 1;
+    for (let i = 0; i < contentarr.length; i++) {
+      contentarr[i] = num + ") " + contentarr[i];
+      num++;
+    } 
+}
+function changeB(){
+    let num = 1;
+    for (let i = 0; i < contentarr.length; i++) {
+      if (contentarr[i] != "") {
+        contentarr[i] = num + ") " + contentarr[i];
+        num++;
+      }
+    }
+}
+// final content
+let finalcontent = "";
+for (let i = 0; i < contentarr.length; i++) {
+  finalcontent += contentarr[i] + "\n";
 }
 console.log(finalcontent);
